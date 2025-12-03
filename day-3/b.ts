1de898accf34608b9629d34fbed30de03d95fab3
elenaalexandra.iorda <elenaalexandra.iordache@bcr.ro>
2025-12-03 10:11:36 +0200

    commented first approach - needs refactoring

--- day-3/b.ts
+++ day-3/b.ts
@@ -2,6 +2,43 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
+
+  // data.forEach((item) => {
+  //
+  //   const numberArray = item.split('').map(Number);
+  //   // console.log(numberArray);
+  //
+  //   // perechi [cifra, index]
+  //   const pairs: [number, number][] = [];
+  //
+  //   for (let i = 0; i < numberArray.length; i++) {
+  //     pairs.push([numberArray[i], i]);
+  //   }
+  //   // console.log(pairs);
+  //
+  //   pairs.sort((a:[number, number], b: [number, number]): number => {
+  //     if (b[0] !== a[0]) return b[0] - a[0];
+  //     return a[1] - b[1]; // tinem cont si de index daca valoarea este aceeasi
+  //   });
+  //   // console.log(pairs);
+  //
+  //   // doar primele 12 perechi
+  //   const smallSetOfPairs: [number, number][] =  pairs.slice(0, 12);
+  //   // console.log(smallSetOfPairs);
+  //
+  //   // ordonam in functie de index acum
+  //   smallSetOfPairs.sort((a: [number, number], b: [number, number] ) => a[1] - b[1]);
+  //   // console.log(smallSetOfPairs);
+  //
+  //   //cream noul numar
+  //   let newNumber = 0;
+  //   for (let i = 0; i < smallSetOfPairs.length; i++) {
+  //     const currentPair = smallSetOfPairs[i];
+  //     newNumber = newNumber * 10 + currentPair[0];
+  //   }
+  //   console.log(newNumber);
+  // })
+
   let finalResult = 0;
 
   data.forEach((item) => {
