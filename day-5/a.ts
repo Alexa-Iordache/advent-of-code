d321d3880dfb7e78cacd7072910b8f346208e398
elenaalexandra.iorda <elenaalexandra.iordache@bcr.ro>
2025-12-05 09:17:04 +0200

    resolve problem 5 for 2025 - first attempt

--- day-5/a.ts
+++ day-5/a.ts
@@ -2,8 +2,39 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day5a(data: string[]) {
-  console.log(data);
-  return 0;
+  // console.log(data);
+
+  const arrayOfNb = []; // array with items to test
+  const freshItems = []; // array with fresh items
+  let result = 0;
+
+  data.forEach(interval => {
+    if(!interval) return;
+
+    const item = interval.split('-').map(Number);
+
+    if(item.length === 1) {
+      arrayOfNb.push(item[0]);
+    } else if(item.length === 2) {
+      let [first, second] = item;
+
+      if (first > second) {
+        [first, second] = [second, first];
+      }
+      freshItems.push([first, second]);
+    }
+  })
+
+  for (const n of arrayOfNb) {
+    for (const [a, b] of freshItems) {
+      if (n >= a && n <= b) {
+        result++;
+        break;
+      }
+    }
+  }
+
+  return result;
 }
 
 await runSolution(day5a);
--- day-5/b.ts
+++ day-5/b.ts
@@ -1,9 +1,66 @@
 import { runSolution } from '../utils.ts';
 
+function mergeIntervals(intervals) {
+  const result = [];
+  let [currentStart, currentEnd] = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const [start, end] = intervals[i];
+
+    if(start <= currentEnd) {
+      currentEnd = Math.max(currentEnd, end);
+    } else {
+      result.push([currentStart, currentEnd]);
+      currentStart = start;
+      currentEnd = end;
+    }
+  }
+
+  // adaugam si ultimul interval
+  result.push([currentStart, currentEnd]);
+  return result;
+}
+
+
 /** provide your solution as the return of this function */
 export async function day5b(data: string[]) {
-  console.log(data);
-  return 0;
+  // console.log(data);
+
+  const freshItems = [];
+  let result = 0;
+
+  data.forEach(interval => {
+    if(!interval) return;
+
+    const item = interval.split('-').map(Number);
+
+    // luam in considerare doar intervalele
+    if(item.length === 2) {
+      let [first, second] = item;
+
+      if (first > second) {
+        [first, second] = [second, first];
+      }
+      freshItems.push([first, second]);
+
+    }
+  })
+  // console.log(freshItems);
+
+  freshItems.sort((first, second) => first[0] - second[0]);
+  const mergedItems = mergeIntervals(freshItems);
+  // console.log(mergedItems);
+
+  for (const [start, end] of mergedItems) {
+     // console.log([start, end]);
+     // for (let i = start; i <= end; i++) {
+     //   result ++;
+     // }
+
+    result += (end-start+1);
+  }
+
+  return result;
 }
 
 await runSolution(day5b);
