779fa431cd4a4ea7ac80e62e1d959846b2ea49c4
elenaalexandra.iorda <elenaalexandra.iordache@bcr.ro>
2025-12-14 16:24:58 +0200

    try to do problem 10 part 2

--- day-10/b.ts
+++ day-10/b.ts
@@ -1,67 +1,81 @@
 import { runSolution } from '../utils.ts';
 
-
-function extractData (line): { buttons, voltage } {
-  const buttonMatches = [...line.matchAll(/\(([0-9,]+)\)/g)];
-  const buttons = buttonMatches.map(m =>
-    m[1].split(",").map(num => parseInt(num, 10))
-  );
-
-  const voltageMatch = line.match(/\{([0-9,]+)\}/);
-  const voltage = voltageMatch
-    ? voltageMatch[1].split(",").map(num => parseInt(num, 10))
-    : [];
-
-  return { buttons, voltage}
+type Machine = {
+  buttons: number[][]
+  voltageCounter: number[]
 }
 
-function calculateMinPresses(buttons: number[][], voltage: number[]): number {
-  const start = Array(voltage.length).fill(0);
+function parseMachineLine(line: string): Machine {
 
-  // state - current value, presses - how many times we clicked on the button
-  const queue: { state: number[], presses: number }[] = [{ state: start, presses: 0 }];
-  const visited = new Set<string>();
-  visited.add(start.join(','));
+  // extract voltage counter (what is inside { })
+  const counterMatch = line.match(/\{([^}]+)}/);
 
-  while (queue.length > 0) {
-    const { state, presses } = queue.shift()!;
+  const voltageCounter = counterMatch[1].split(",").map(Number);
 
-    // check if the target was reached
-    if (state.every((v, i) => v === voltage[i])) {
-      console.log(presses)
-      return presses;
-    }
+  // extract all buttons (what is inside ( ))
+  const buttonMatch = line.match(/\(([^)]+)\)/g) || [];
 
+  const buttons = buttonMatch.map(btn =>
+    btn
+      .slice(1, -1) // remove ( )
+      .split(",")
+      .map(Number)
+  );
+
+  return { buttons, voltageCounter };
+}
+
+
+function minButtonPressesForMachine(buttons, voltageCounter) {
+  const copyVoltageCounter = [...voltageCounter];
+
+  // sort buttons from max length to min length
+  buttons.sort((a, b) => b.length - a.length);
+
+  let presses = 0;
+  let changed = true;
+
+  // loop continues until no button can be pressed anymore
+  while (changed) {
+    changed = false;
+
+    // try each button
     for (const button of buttons) {
-      const newState = state.slice(); // make a copy of the current state
-      for (const idx of button) {
-        // we increment the state of the voltage based on the button pressing, but without exceeding the desired voltage
-        newState[idx] = Math.min(voltage[idx], newState[idx] + 1);
-      }
+      let minValue = Number.MAX_SAFE_INTEGER;
+      // console.log(button);
 
-      const key = newState.join(',');
-      if (!visited.has(key)) {
-        visited.add(key);
-        queue.push({ state: newState, presses: presses + 1 });
+      for (const idx of button) {
+        // console.log(idx);
+        minValue = Math.min(minValue, copyVoltageCounter[idx]);
+      }
+      // console.log('\n');
+
+      if (minValue > 0 && minValue < Number.MAX_SAFE_INTEGER) {
+        presses += minValue;
+        changed = true;
+
+        for (const idx of button) {
+          copyVoltageCounter[idx] -= minValue;
+        }
       }
     }
   }
 
-  return Infinity;
+  for (let i = 0; i < copyVoltageCounter.length; i++) {
+    presses += copyVoltageCounter[i];
+  }
+
+  return presses;
 }
+
 
 /** provide your solution as the return of this function */
 export async function day10b(data: string[]) {
-  // console.log(data);
   let result = 0;
 
-  data.forEach((line) => {
-    const lines = extractData(line);
-
-    console.log(lines.buttons, lines.voltage);
-
-    // console.log(calculateMinPressesJoltage(lines.buttons, lines.voltage));
-    result += calculateMinPresses(lines.buttons, lines.voltage)
+  data.forEach(line => {
+    const { buttons, voltageCounter } = parseMachineLine(line);
+    result += minButtonPressesForMachine(buttons, voltageCounter);
   });
 
   return result;
